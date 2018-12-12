231124105339f468ada3d402fa220898274e8c30
Nicole Wilsey Starr <Nicole.WilseyStarr@gmail.com>
2018-12-12 14:57:51 -0600

    made logic changes to finding the correct letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,63 +7,60 @@ var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe", "Jack
     "Nixon", "Ford", "Carter", "Reagan", "Bush", "Clinton", "Obama", "Trump"];
 
 //Create variables for tracking 
-var guessingWordText = document.getElementById("guessing-word");
+var blanks = document.getElementById("blanks");
+var correctGuess = document.getElementById("correct-letters");
 var correctLetters = [];
-var incorrectLetters = [];
-var maxGuesses = 5;
 var currentWord = document.getElementById("current-word");
 var currentWordText = document.getElementById("currentword-text");
-var wins = 0;
-var remainingGuesses = 0;
-var correctGuess = document.getElementById("correct-letters");
-var incorrectGuess = document.getElementById("incorrect-letters");
-var wrongLetters = [];
-var splitWord = [];
-var replaceText = document.getElementById("replace-text");
+var guessedLetters = [];
 var guessingWord = [];
+var guessingWordText = document.getElementById("guessing-word");
+var incorrectGuess = document.getElementById("incorrect-letters");
+var incorrectLetters = [];
+var maxGuesses = 5;
+var remainingGuesses = 0;
+var replaceText = document.getElementById("replace-text");
+var splitWord = [];
+var wins = 0;
+var wrongLetters = [];
 
-//Load computer word before the guesses begin
+//Load word to guess before the user begins guessing
 var currentWord = presidents[Math.floor(Math.random() * presidents.length)];
 var currentWord = currentWord.toLowerCase();
 console.log(currentWord);
 
-//Display _ _ _ _ _ for the number of characters in the name 
-
-
- // Build the guessing word and clear it out
+//Display _ _ _ _ _ for the number of characters in the name, before a keystroke
  for (var l = 0; l < currentWord.length; l++) {
-    guessingWord.push("_");
+    guessingWord.push("_"); {
+        (blanks.textContent = guessingWord);
+    };
 }   
-console.log(guessingWord);
 
-//Check the user guess against the current word 
-//All lowercase .toLowerCase 
-// function splitWords() {
-    var splitWord = currentWord.split("");
-    console.log(splitWord);
+//Splitting the word into a string of separate letters
+var splitWord = currentWord.split("");
+console.log(splitWord);
 
 //onkeyevent to track users key strokes
 document.onkeyup = function (event) {
     var userGuess = event.key;
     guessedLetters.push(userGuess);
-    var i;
-    for (i = 0; i < splitWord.length; i++) {
-        if (guessedLetters.includes(splitWord[i])) {
-        (correctLetters[i] = splitWord[i]);
-        } (correctGuess.textContent = "The answer is " + correctLetters);
-    };
+    //Checks the current letter guessed against the array
+    var correctLetters = splitWord.includes(userGuess);
+    //console logs the letter guessed
+    console.log(userGuess); 
+    //Displys the boolean of True or false, changes with each keystroke
+    (correctGuess.textContent = "The answer is " + correctLetters);
 }
-    // var j;
-    // for (j = 0; j < splitWord.length; j++) {
-    //     if (guessedLetters.includes(splitWord[j])) {
-    //     incorrectLetters[j] = !splitWord[j];
-    //     } incorrectGuess.textContent = "The incorrect letters are " + incorrectLetters;
-    // }; 
 
+//Need if else statement. 
+//================================================
+//If the guess is true display it at the proper index in the correctLetters array. ??Need additional 
+
+//Else, log display it in the "incorrectGuess" array
 
 // have an array that has each ch the user is trying to guess, and have an array of the letters the user has actually guessed, 
 // and an empty array loop through the letters that they are guessing
-//and we check if it's there, then plug it in the empty array... put this in index 2 in the array
+//and we check if it's there, then plug it in the empty array... put this in index in the array
 
 //Array of the current word 
 
